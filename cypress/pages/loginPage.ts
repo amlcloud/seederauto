0f1b501a290362f05151718a2c352a84b06f66ce
PadmashreeJayaram <padmashreegowda121@gmail.com>
2022-08-15 07:35:48 +1000

    updated the script as per new login screen

--- cypress/pages/loginPage.ts
+++ cypress/pages/loginPage.ts
@@ -4,13 +4,13 @@ export class LoginPage {
 
   public clickLoginanonymous() {
     cy.get('flt-semantics-placeholder').first().click({ force: true });
-    cy.contains('Please Login');
-    cy.findByFltSemanticsAriaLabel('log-in Anonymous').click({ force: true });
+    cy.contains('Log in');
+    cy.findByFltSemanticsAriaLabel('Log in Anonymous').click({ force: true });
     cy.contains('ENTITIES');
   }
   public logOut(){
     cy.contains('ENTITIES');
     cy.findByFltSemanticsAriaLabel('sign out').click({ force: true });
-    cy.contains('Please Login');
+    cy.contains('Log in');
   }
 }
